89f74aa77bcde52d9dae440c1a6956827ccd0df3
Angie Newbie <angel890817@hotmail.com>
2025-04-12 08:53:08 +0900

    title shown

--- pokemon.js
+++ pokemon.js
@@ -7,10 +7,23 @@
 // 3. If request succeccds, resolve the promise with the result
 // 4. Otherwise, reject the promise with an error
 
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase()
+    +this.slice(1)
+}
+    
+
 fetch('https://pokeapi.co/api/v2/pokemon/ditto')
 // this then handle the promise return by fetch
     .then(response => response.json()
         // this then hadles the promise return by text
-            .then(data => console.log(data.name))
+            .then(data => {
+                // update the text of pokemon name with the name from the data
+                // 1. get the dom node for pokemon name
+                const pokemonName = document.querySelector('#pokemon-name') 
+                // 2. Set innerText on pokemonName to data.name
+                data.name[0] = data.name[0].toUpperCase()
+                
+            })
 )
 console.log('Awaiting fetch...')
